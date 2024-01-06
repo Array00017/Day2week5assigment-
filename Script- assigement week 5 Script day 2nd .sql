429381cc40629de92e1ec397c8e62b40ead7b7cf
Arnold <151367062+Array00017@users.noreply.github.com>
2024-01-06 12:42:22 -0500

    Add files via upload

---
+++ Script- assigement week 5 Script day 2nd .sql
@@ -0,0 +1,58 @@
+create table customer 
+(
+customer_id primary key 
+name varchar(50)
+address varchar(50)
+
+);
+
+create table tickets
+(
+ticket_id int primary key 
+purchase price int 
+
+);
+
+
+create table movies
+(
+movie_id int primary key 
+title varchar(50)
+genere varchar(50)
+movie_length int 
+
+);
+
+
+from customer 
+select name 
+insert * 'Jerry'
+
+from customer 
+select address 
+insert * '100 road'
+
+
+from tickets 
+select purchase price 
+insert* int (25)
+as $$
+
+
+from movies 
+select title 
+insert * 'I Am Legend'
+
+from movies 
+select genere 
+insert * 'Action'
+
+
+from movies 
+select movie_length
+insert * int (3) 'hours'
+
+
+
+
+
